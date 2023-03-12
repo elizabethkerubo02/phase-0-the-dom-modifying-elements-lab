37bbc565e5a80fd79ce0559de7e79ecf0b5132e3
elizabethkerubo02 <keruboelizabeth02@gmail.com>
2023-03-12 21:05:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "Elizabeth";
+newHeader.innerHTML ="your name is means godly";
+
+
+
+
+
+
